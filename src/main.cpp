f83deb5248d05432c1d25207b2d3ae1c2efd89ae
adem dali <dalyademoff@gmail.com>
2026-02-13 21:31:58 +0100

    added ackerman

--- src/main.cpp
+++ src/main.cpp
@@ -49,7 +49,7 @@
 #define LEFT_VEL_KI 0.007
 #define LEFT_VEL_KD 0.0
 
-#define STEERING_KP 1.2
+#define STEERING_KP 1.0
 #define STEERING_KI 0.0
 #define STEERING_KD 0.0
 
@@ -68,7 +68,7 @@
 #define RIGHT_ENCODER_CPR 408
 #define LEFT_WHEEL_DIAMETER_MM 67.58 //arbitrary number //65 //91.77 //72.19
 #define RIGHT_WHEEL_DIAMETER_MM 67.58 //65 //99.32
-#define WHEEL_BASE_MM 150 //distance between wheels
+#define WHEEL_BASE_MM 194 //distance between wheels
 #define SERVO_INIT_ANGLE 87 //87
 /********************** ODOMETRY VARIABLES ********************* */
 volatile double left_wheel_curr_vel_mm_s, right_wheel_curr_vel_mm_s, robot_curr_vel_mm_s;
@@ -84,7 +84,7 @@ volatile uint32_t last_vel_calc_ms = 0;
 volatile float right_vel_kp = RIGHT_VEL_KP, right_vel_ki = RIGHT_VEL_KI, right_vel_kd = RIGHT_VEL_KD;
 volatile float left_vel_kp = LEFT_VEL_KP, left_vel_ki = LEFT_VEL_KI, left_vel_kd = LEFT_VEL_KD;
 volatile float steering_kp = STEERING_KP, steering_ki = STEERING_KI, steering_kd = STEERING_KD;
-
+volatile uint8_t K_heading = 8;
 /**************** ACTUATORS VARIABLES ********** */
 volatile int32_t right_motor_cmd, left_motor_cmd;
 volatile int16_t servo_angle_cmd_deg = 93 ; //in deg
@@ -313,16 +313,39 @@ void SetServoAngle(){
 }
 */
 void SetServoAngle(){
-  servo_angle_cmd_deg = vision.get_servo_angle(last_camera_angle);
-  servo_angle_cmd_deg = (180 - servo_angle_cmd_deg);
-  if (servo_angle_cmd_deg >= 87){
-    servo_angle_cmd_deg = servo_angle_cmd_deg * STEERING_KP;
+
+  float L = WHEEL_BASE_MM / 1000.0;  // meters
+
+  // heading error (rad)
+  float heading_error_rad = radians(last_camera_angle - 87.0);
+
+  // curvature
+  float curvature = K_heading * heading_error_rad;
+
+  // steering wheel angle (rad)
+  float steering_rad = atan(L * curvature);
+
+  // convert to degrees
+  float steering_deg = degrees(steering_rad);
+  
+  // optional steering gain
+  
+
+  // convert to servo position
+  float servo_angle = 87 + steering_deg;
+
+  servo_angle = 180 - servo_angle;
+  if (servo_angle>=87){
+    servo_angle *= STEERING_KP;
   }
   else {
-    servo_angle_cmd_deg = servo_angle_cmd_deg * (1/STEERING_KP);
+    servo_angle *= 1/STEERING_KP;
   }
-  servo_angle_cmd_deg = constrain(servo_angle_cmd_deg, MIN_SERVO_ANGLE, MAX_SERVO_ANGLE);
-  steer_servo.write(servo_angle_cmd_deg);
+
+  // constrain
+  servo_angle = constrain(servo_angle, MIN_SERVO_ANGLE, MAX_SERVO_ANGLE);
+
+  steer_servo.write((int)servo_angle);
 }
 
 
